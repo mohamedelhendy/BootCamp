218f1f1eef9646acd13012b19c9b611044f4521c
mohamed elhendy <74493245+mohamedelhendy@users.noreply.github.com>
2022-12-20 03:50:41 +0200

    task 4 angular (cartlines & cart logics => oop)

---
+++ angular 2/online-shop/src/app/cart.ts
@@ -0,0 +1,23 @@
+import { CartLine } from "./interfaces/cart-line";
+import { StorageService } from 'src/app/services/storage.service';
+
+export class Cart  {
+    constructor(private cartline:CartLine,
+    private storageService:StorageService){
+
+    }
+    cartLines: CartLine[]=this.storageService.getCartLines();
+    getTotal(): number {
+        return this.getShipping() + this.getSubTotal();
+      }
+      getSubTotal(): number {
+        return this.cartLines
+          .map((x) => x.price * x.quantity)
+          .reduce((a, v) => (a += v), 0);
+      }
+      getShipping(): number {
+        return (
+          this.cartLines.map((x) => x.quantity).reduce((a, v) => (a += v), 0) * 2
+        );
+      }
+}
--- angular 2/online-shop/src/app/components/layouts/cart/cart.component.ts
+++ angular 2/online-shop/src/app/components/layouts/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cart } from 'src/app/cart';
 import { CartLine } from 'src/app/interfaces/cart-line';
 import { StorageService } from 'src/app/services/storage.service';
 
@@ -8,22 +9,19 @@ import { StorageService } from 'src/app/services/storage.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-  constructor(private storageService: StorageService) {
+  constructor(private storageService: StorageService,
+    private cart:Cart) {
     this.cartLines = storageService.getCartLines();
   }
   cartLines: CartLine[] = [];
 
   getTotal(): number {
-    return this.getShipping() + this.getSubTotal();
+    return this.cart.getTotal();
   }
   getSubTotal(): number {
-    return this.cartLines
-      .map((x) => x.price * x.quantity)
-      .reduce((a, v) => (a += v), 0);
+    return this.cart.getSubTotal();
   }
   getShipping(): number {
-    return (
-      this.cartLines.map((x) => x.quantity).reduce((a, v) => (a += v), 0) * 2
-    );
+    return this.cart.getShipping();
   }
 }

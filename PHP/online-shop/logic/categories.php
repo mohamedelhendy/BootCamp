f7396e027b395fc14325dacbbee6ee995affb5d8
mohamed elhendy <74493245+mohamedelhendy@users.noreply.github.com>
2023-01-03 04:46:07 +0200

    PHP task 1 fix

--- PHP/online-shop/logic/categories.php
+++ PHP/online-shop/logic/categories.php
@@ -1,7 +1,35 @@
 <?php
-require_once(BASE_PATH.'dal/dal.php');
 function getCategories()
 {
-    return get_rows("SELECT c.*,IFNULL(p.product_count,0) product_count FROM categories c 
-    LEFT JOIN (SELECT COUNT(0) product_count,category_id FROM products) p ON c.id=p.category_id");
+    $categories = [
+        [
+            'id' => 1,
+            'name' => 'Clothes',
+            'image' => 'img/cat-1.jpg',
+            'product_count' => 100,
+            'image_url' => 'img/cat-1.jpg'
+        ],
+        [
+            'id' => 2,
+            'name' => 'Electronics',
+            'image' => 'img/cat-2.jpg',
+            'product_count' => 100,
+            'image_url' => 'img/cat-2.jpg'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Shoes',
+            'image' => 'img/cat-3.jpg',
+            'product_count' => 100,
+            'image_url' => 'img/cat-3.jpg'
+        ],
+        [
+            'id' => 4,
+            'name' => 'Cosmotics',
+            'image' => 'img/cat-4.jpg',
+            'product_count' => 100,
+            'image_url' => 'img/cat-4.jpg'
+        ],
+    ];
+    return $categories;
 }
--- PHP/online-shop/logic/products.php
+++ PHP/online-shop/logic/products.php
@@ -1,15 +1,14 @@
 <?php
-require_once(BASE_PATH.'dal/dal.php');
 function getProducts(){
     $products = [
-        ['name' => 'Product1', 'img' => 'img/product-1.jpg', 'price' => 100,'discount'=>0.1,'rating'=>0,'rating_count'=>100,'is_featured'=>true,'is_recent'=>false],
-        ['name' => 'Product2', 'img' => 'img/product-2.jpg', 'price' => 110,'discount'=>0.125,'rating'=>1,'rating_count'=>90,'is_featured'=>true,'is_recent'=>false],
-        ['name' => 'Product3', 'img' => 'img/product-3.jpg', 'price' => 120,'discount'=>0.2,'rating'=>1.5,'rating_count'=>95,'is_featured'=>true,'is_recent'=>false],
-        ['name' => 'Product4', 'img' => 'img/product-4.jpg', 'price' => 130,'discount'=>0.1,'rating'=>4.5,'rating_count'=>100,'is_featured'=>true,'is_recent'=>false],
-        ['name' => 'Product5', 'img' => 'img/product-5.jpg', 'price' => 140,'discount'=>0.1,'rating'=>3.5,'rating_count'=>97,'is_featured'=>false,'is_recent'=>true],
-        ['name' => 'Product6', 'img' => 'img/product-6.jpg', 'price' => 150,'discount'=>0.2,'rating'=>3,'rating_count'=>99,'is_featured'=>false,'is_recent'=>true],
-        ['name' => 'Product2', 'img' => 'img/product-2.jpg', 'price' => 160,'discount'=>0.3,'rating'=>4,'rating_count'=>98,'is_featured'=>false,'is_recent'=>true],
-        ['name' => 'Product1', 'img' => 'img/product-1.jpg', 'price' => 170,'discount'=>0.2,'rating'=>5,'rating_count'=>99,'is_featured'=>false,'is_recent'=>true],
+        ['name' => 'Product1', 'image_url' => 'img/product-1.jpg', 'price' => 100,'discount'=>0.1,'rating'=>0,'rating_count'=>100,'is_featured'=>true,'is_recent'=>false],
+        ['name' => 'Product2', 'image_url' => 'img/product-2.jpg', 'price' => 110,'discount'=>0.125,'rating'=>1,'rating_count'=>90,'is_featured'=>true,'is_recent'=>false],
+        ['name' => 'Product3', 'image_url' => 'img/product-3.jpg', 'price' => 120,'discount'=>0.2,'rating'=>1.5,'rating_count'=>95,'is_featured'=>true,'is_recent'=>false],
+        ['name' => 'Product4', 'image_url' => 'img/product-4.jpg', 'price' => 130,'discount'=>0.1,'rating'=>4.5,'rating_count'=>100,'is_featured'=>true,'is_recent'=>false],
+        ['name' => 'Product5', 'image_url' => 'img/product-5.jpg', 'price' => 140,'discount'=>0.1,'rating'=>3.5,'rating_count'=>97,'is_featured'=>false,'is_recent'=>true],
+        ['name' => 'Product6', 'image_url' => 'img/product-6.jpg', 'price' => 150,'discount'=>0.2,'rating'=>3,'rating_count'=>99,'is_featured'=>false,'is_recent'=>true],
+        ['name' => 'Product2', 'image_url' => 'img/product-2.jpg', 'price' => 160,'discount'=>0.3,'rating'=>4,'rating_count'=>98,'is_featured'=>false,'is_recent'=>true],
+        ['name' => 'Product1', 'image_url' => 'img/product-1.jpg', 'price' => 170,'discount'=>0.2,'rating'=>5,'rating_count'=>99,'is_featured'=>false,'is_recent'=>true],
     ];
     return $products;
 }
